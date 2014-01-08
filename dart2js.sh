0615bdd6c977541b67c8cb4b19c6c428d509fce6
Pierre Mage <pierre.mage@gmail.com>
2014-01-08 18:26:59 +0000

    update Dart to version 1.0

--- dart2js.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-dart2js -c web/SpeedReader.dart -oweb/SpeedReader.dart.js
--- web/SpeedReader.dart
+++ web/SpeedReader.dart
@@ -1,16 +1,9 @@
 library speedreader;
 
-import 'dart:coreimpl';
-import 'dart:isolate';
+import 'dart:async';
 import 'dart:html';
 
-final WHITESPACES = new JSSyntaxRegExp('\\s+');
-final P_KEY = 'p'.charCodeAt(0);
-final R_KEY = 'r'.charCodeAt(0);
-final K_KEY = 'k'.charCodeAt(0);
-final J_KEY = 'j'.charCodeAt(0);
-final H_KEY = 'h'.charCodeAt(0);
-final L_KEY = 'l'.charCodeAt(0);
+final WHITESPACES = new RegExp('\\s+');
 
 class SpeedReader { 
   Timer currentTimer;
@@ -36,31 +29,30 @@ class SpeedReader {
   }
 
   void run() {
-    document.query('#read').on.click.add((Event e) { 
-      setText(document.query('#source').value);
+    querySelector('#read').onClick.listen((e) { 
+      setText((querySelector('#source') as TextAreaElement).value);
       reading = true;
       read();
     });
-    window.on.keyPress.add((Event e)
-    {
-      switch (e.charCode) {
-        case P_KEY:
+    window.onKeyPress.listen((e) {
+      switch (e.keyCode) {
+        case KeyCode.P:
           reading = !reading;
           read();  
           break;
-        case R_KEY:
+        case KeyCode.R:
           restart();
           break;
-        case K_KEY:
+        case KeyCode.K:
           setWordsPerMinute(wordsPerMinute + 10);
           break;
-        case J_KEY:
+        case KeyCode.J:
           setWordsPerMinute(wordsPerMinute - 10);
           break;
-        case H_KEY:
+        case KeyCode.H:
           rewind();
           break;
-        case L_KEY:
+        case KeyCode.L:
           forward();
           break;
       }
@@ -97,17 +89,17 @@ class SpeedReader {
       return;
     }
     int periodInMilliSeconds = 1000 ~/ (wordsPerMinute ~/ 60);
- 	  currentTimer = new Timer.repeating(periodInMilliSeconds, (Timer t) {
+    currentTimer = new Timer.periodic(new Duration(milliseconds: periodInMilliSeconds), (Timer t) {
       forward();
       if (!reading || currentPosition >= text.chunksLength()) {
-      	t.cancel();
+        t.cancel();
         reading = false;
-  	  }
-  	}); 
+      }
+    }); 
   }
   
   void displayChunk(int position) {
-    document.query('#text').innerHTML = text.chunkAt(position);
+    querySelector('#text').setInnerHtml(text.chunkAt(position));
   }
 
   void restart() {
